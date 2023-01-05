e2646b4cb322f78f64c31e64cf765a8aae8d4b06
Nandhu S <nandhus1810@gmail.com>
2023-01-05 21:10:08 +0530

    automatic changes

--- woc/src/index.js
+++ woc/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import 'remixicon/fonts/remixicon.css'
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
