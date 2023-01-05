06d1afdda86272106698205f2208d347e062800e
Nandhu S <nandhus1810@gmail.com>
2023-01-05 21:11:56 +0530

    added bottom banner

---
+++ woc/src/components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './banner.css'
+
+
+const Banner = () => {
+  return <section>
+    <div className='banner-container'>
+        <div className='banner-text'>
+            <h1>Join 3000+ participants worldwide<br/>and achieve your dream!</h1>
+        </div>
+        <div className='banner-btn'>
+            <button className='join-us-now'>Join us now!</button>
+        </div>
+        
+    </div>
+    </section>
+}
+
+export default Banner
